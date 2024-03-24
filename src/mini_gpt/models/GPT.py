46f25d0be4064007d76e4e189381403c6d4fb5c7
lorenzocollodi <lorenzocollodi@gmail.com>
2024-03-24 10:53:21 +0000

    Correct sizes for MultiHeadAttention

--- src/mini_gpt/models/GPT.py
+++ src/mini_gpt/models/GPT.py
@@ -1,28 +1,38 @@
-from torch import nn, Tensor, sqrt, tensordot, zeros
+from torch import Tensor, nn, sqrt, zeros
 
 
-class AttentionBlock(nn.Module):
-    def __init__(self, input_size: int = 1024, k_size: int = 64, heads: int = 8, output_size: int = 1024):
+class MultiHeadAttentionBlock(nn.Module):
+    def __init__(
+        self,
+        input_size: int = 1024,
+        k_size: int = 64,
+        heads: int = 8,
+        output_size: int = 1024,
+    ):
+        super().__init__()
         self.input_size = input_size
         self.v_size = output_size // heads
         self.k_size = k_size
         self.heads = heads
         self.output_size = output_size
-        self.Mq = zeros((self.heads, 1, self.input_size, self.k_size))
-        self.Mk = zeros((self.heads, 1, self.input_size, self.k_size))
-        self.Mv = zeros((self.heads, 1, self.input_size, self.v_size))    
-        self._parameters = {"Mq": self.Mq, "Mk": self.Mk, "Mv": self.Mv}
-        self._modules = {}
+        self.Mq = nn.Parameter(zeros(self.input_size, self.heads * self.k_size))
+        self.Mk = nn.Parameter(zeros(self.input_size, self.heads * self.k_size))
+        self.Mv = nn.Parameter(zeros(self.input_size, self.heads * self.v_size))
 
     def forward(self, x: Tensor):
-        q = x @ self.Mq
-        print(q.shape)
-        k = x @ self.Mk
-        print(k.shape)
-        v = x @ self.Mv
-        print(v.shape)
-        head_weights = nn.functional.softmax(tensordot(q, k.T) / sqrt(self.k_size), dim=1)
-        return head_weights @ v.T
+        B, C, _ = x.shape
+        q = (x @ self.Mq).view(B, self.heads, C, -1)
+        assert isinstance(q, Tensor)
+        k = (x @ self.Mk).view(B, self.heads, C, -1)
+        assert isinstance(k, Tensor)
+        v = (x @ self.Mv).view(B, self.heads, C, -1)
+        assert isinstance(v, Tensor)
+        product = q.view(B * self.heads, C, -1) @ k.permute(0, 1, 3, 2).view(
+            B * self.heads, -1, C
+        )
+        normalised_product = product / sqrt(Tensor([self.k_size]))
+        head_weights = nn.functional.softmax(normalised_product, dim=1)
+        return (head_weights.view(B, C, self.heads, -1) @ v).view(B, C, -1)
 
 
 class GPT(nn.Module):
@@ -32,8 +42,8 @@ class GPT(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         pass
 
+
 if __name__ == "__main__":
-    block = AttentionBlock()
+    block = MultiHeadAttentionBlock()
     input = zeros((4, 8, 1024))
-    block.forward(input)
-    print([param for param in block.parameters()])
+    print(block.forward(input).shape)
--- src/mini_gpt/train.py
+++ src/mini_gpt/train.py
@@ -5,5 +5,6 @@ def train():
     dataset = TokensDataset(8, 4, "models/tokenizer.json", "data/train.txt")
     print(dataset.get_batch(3))
 
+
 if __name__ == "__main__":
     train()
