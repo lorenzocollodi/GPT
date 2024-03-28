e3c337fb3254b2a742ab5f91090f39f962c916e1
lorenzocollodi <lorenzocollodi@gmail.com>
2024-03-28 14:47:53 +0100

    Add scalar logging

--- src/mini_gpt/train.py
+++ src/mini_gpt/train.py
@@ -4,15 +4,18 @@ from transformers import PreTrainedTokenizerFast
 from mini_gpt.datasets.batch_operators import expand_gt, get_mask, time_expand
 from mini_gpt.datasets.dataset import TokensDataset
 from mini_gpt.models.GPT import GPT
+from torch.utils.tensorboard import SummaryWriter
 
 TOKENIZER_PATH = "models/tokenizer.json"
 TRAIN_DATA_PATH = "data/train.txt"
-N_EPOCHS = 1000
+N_EPOCHS = 100
+N_STEPS = 1000
 B_SIZE = 4
 CONTEXT_LENGTH = 8
 
 
 def train():
+    writer = SummaryWriter()
     tokenizer = PreTrainedTokenizerFast(tokenizer_file=TOKENIZER_PATH)
     dataset = TokensDataset(CONTEXT_LENGTH, TRAIN_DATA_PATH, tokenizer)
     gpt_model = GPT()
@@ -21,23 +24,26 @@ def train():
         param.requires_grad = True
     gpt_model.train()
     optimizer = optim.AdamW(gpt_model.parameters())
-    for _ in range(N_EPOCHS):
-        batch_idxs = randint(high=len(dataset) - CONTEXT_LENGTH, size=(B_SIZE,))
+    for epoch in range(N_EPOCHS):
+        for step in range(N_STEPS):
+            step = epoch*N_STEPS + step
+            batch_idxs = randint(high=len(dataset) - CONTEXT_LENGTH, size=(B_SIZE,))
 
-        batch_x, batch_y = dataset.get_batch(batch_idxs)
-        batch_y = expand_gt(batch_x, batch_y)
-        batch_x = time_expand(batch_x)
-        mask = get_mask(batch_x)
-        loss = gpt_model.forward(
-            batch_x.reshape(-1, CONTEXT_LENGTH),
-            batch_y,
-            mask.reshape(-1, CONTEXT_LENGTH),
-        )
-        for param in gpt_model.parameters():
-            param.grad = None
-        loss.backward()
-        optimizer.step()
-        print(loss)
+            batch_x, batch_y = dataset.get_batch(batch_idxs)
+            batch_y = expand_gt(batch_x, batch_y)
+            batch_x = time_expand(batch_x)
+            mask = get_mask(batch_x)
+            loss = gpt_model.forward(
+                batch_x.reshape(-1, CONTEXT_LENGTH),
+                batch_y,
+                mask.reshape(-1, CONTEXT_LENGTH),
+            )
+            writer.add_scalar("loss/train", loss, global_step=step)
+            for param in gpt_model.parameters():
+                param.grad = None
+            loss.backward()
+            optimizer.step()
+            print(loss)
 
 
 if __name__ == "__main__":
