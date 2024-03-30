b8390e9e1f3832bf56bd6b61ffc4410dcbb52729
lorenzocollodi <lorenzocollodi@gmail.com>
2024-03-30 15:23:05 +0100

    Write and parse train config

--- src/mini_gpt/train.py
+++ src/mini_gpt/train.py
@@ -1,42 +1,54 @@
+import argparse
+
 from torch import cuda, device, no_grad, optim, randint
 from torch.utils.tensorboard import SummaryWriter
 from transformers import PreTrainedTokenizerFast
 
+from mini_gpt.configs import TrainConfig
 from mini_gpt.datasets.batch_operators import expand_gt, get_mask, time_expand
 from mini_gpt.datasets.dataset import TokensDataset
 from mini_gpt.models.GPT import GPT
 
-TOKENIZER_PATH = "models/tokenizer.json"
-TRAIN_DATA_PATH = "data/train.txt"
-VAL_DATA_PATH = "data/val.txt"
-N_EPOCHS = 100
-N_STEPS = 1000
-B_SIZE = 4
-CONTEXT_LENGTH = 8
+
+def parse_arguments() -> TrainConfig:
+    parser = argparse.ArgumentParser(
+        prog="GPT trainer", description="Train your own GPT model"
+    )
+    parser.add_argument("--tokenizer", type=str, default="models/tokenizer.json")
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--val-every", type=int, default=1000)
+    parser.add_argument("--batch-size", type=int, default=4)
+    parser.add_argument("--context-length", type=int, default=8)
+    parser.add_argument("--train-data", type=str, default="data/train.txt")
+    parser.add_argument("--val-data", type=str, default="data/val.txt")
+    arguments = {key: value for key, value in parser.parse_args()._get_kwargs()}
+    return TrainConfig(**arguments)
 
 
-def train():
+def train(args: TrainConfig):
     if cuda.is_available():
         data_device = device("cuda")
     else:
         data_device = device("cpu")
 
     writer = SummaryWriter()
-    tokenizer = PreTrainedTokenizerFast(tokenizer_file=TOKENIZER_PATH)
+    tokenizer = PreTrainedTokenizerFast(tokenizer_file=args.tokenizer)
     train_dataset = TokensDataset(
-        CONTEXT_LENGTH, TRAIN_DATA_PATH, tokenizer, data_device
+        args.context_length, args.train_data, tokenizer, data_device
     )
-    val_dataset = TokensDataset(CONTEXT_LENGTH, VAL_DATA_PATH, tokenizer, data_device)
+    val_dataset = TokensDataset(
+        args.context_length, args.val_data, tokenizer, data_device
+    )
     gpt_model = GPT().to(data_device)
     for param in gpt_model.parameters():
         param.requires_grad = True
     optimizer = optim.AdamW(gpt_model.parameters())
-    for epoch in range(N_EPOCHS):
+    for epoch in range(args.epochs):
         gpt_model.train()
-        for step in range(N_STEPS):
-            step = epoch * N_STEPS + step
+        for step in range(args.val_every):
+            step = epoch * args.val_every + step
             batch_idxs = randint(
-                high=len(train_dataset) - CONTEXT_LENGTH, size=(B_SIZE,)
+                high=len(train_dataset) - args.context_length, size=(args.batch_size,)
             )
 
             batch_x, batch_y = train_dataset.get_batch(batch_idxs)
@@ -44,9 +56,9 @@ def train():
             batch_x = time_expand(batch_x)
             mask = get_mask(batch_x)
             loss = gpt_model.forward(
-                batch_x.reshape(-1, CONTEXT_LENGTH),
+                batch_x.reshape(-1, args.context_length),
                 batch_y,
-                mask.reshape(-1, CONTEXT_LENGTH),
+                mask.reshape(-1, args.context_length),
             )
             writer.add_scalar("loss/train", loss, global_step=step)
             for param in gpt_model.parameters():
@@ -63,13 +75,14 @@ def train():
                 batch_x = time_expand(batch_x.unsqueeze(0))
                 mask = get_mask(batch_x)
                 val_loss += gpt_model.forward(
-                    batch_x.reshape(-1, CONTEXT_LENGTH),
+                    batch_x.reshape(-1, args.context_length),
                     batch_y,
-                    mask.reshape(-1, CONTEXT_LENGTH),
+                    mask.reshape(-1, args.context_length),
                 )
             val_loss /= len(val_dataset)
             writer.add_scalar("loss/val", val_loss, global_step=epoch)
 
 
 if __name__ == "__main__":
-    train()
+    args = parse_arguments()
+    train(args)
